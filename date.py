3e5f8e4279fc2757589b526e51d2e4440d3079fc
fatemeh <fateme.mahavianpoor@gmail.com>
2023-06-01 20:43:47 +0330

    new changes in date file

--- date.py
+++ date.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime 
 import sqlite3
 import functions
 from constants.constant import db_names,table_names
@@ -7,8 +7,10 @@ from constants.constant import db_names,table_names
 
 
 time_module= datetime.datetime.now()
-time=(time_module.strftime("%X"))
+
+timer=(time_module.strftime("%X"))
 date=(time_module.strftime("%x"))
+
 
 class Date:
 
@@ -21,9 +23,9 @@ class Date:
         cursor_sql=sql_connect.cursor()
        
         cursor_sql.execute("SELECT * FROM products ")
-        items=cursor_sql.fetchall() 
+        table=cursor_sql.fetchall() 
         
-        for item in items:
+        for item in table:
                 product_id=int(id)
                 if item[1]==product_id:
                     
@@ -33,12 +35,14 @@ class Date:
                     items=cursor_sql.fetchall()
 
                     for row in items:
-                        if date == row[1]:
+                        if date == row[1] and item[1]==product_id :
                             print("You're product have already entered!") 
                             break
                     else:
-                        list=[id,date,item[2],time]
+                        list=[id,date,item[2],timer]
+                        
                         functions.add_many(list,db_names["date"],table_names["table5"],len(list))
+                        break
 
     def exit_product(self):
          
@@ -48,9 +52,9 @@ class Date:
         cursor_sql=sql_connect.cursor()
        
         cursor_sql.execute("SELECT * FROM products ")
-        items=cursor_sql.fetchall() 
+        table=cursor_sql.fetchall() 
         
-        for item in items:
+        for item in table:
                 product_id=int(id)
                 if item[1]==product_id:
                     
@@ -60,12 +64,14 @@ class Date:
                     items=cursor_sql.fetchall()
 
                     for row in items:
-                        if date == row[1]:
+                        if date == row[1] and item[1]==product_id :
                             print("You're product have already entered!") 
                             break
                     else:
-                        list=[id,date,item[2],time]
+                        list=[id,date,item[2],timer]
+                        
                         functions.add_many(list,db_names["date"],table_names["table7"],len(list))
+                        break
                                   
     def enter_user(self):
         
@@ -92,52 +98,52 @@ class Date:
                         if  date == row[0] and item[1]==product_id:
                             print("You're employer have already entered!")
                             break
-                else:
-                    list=[date,id,item[2],item[3],time,'']
-                    functions.add_many(list,db_names["date"],table_names["table6"],len(list))
-                    break         
-        
-                                    
-   
-
+                    else:
+                        list=[date,id,item[2],item[3],timer,'']
+                        functions.add_many(list,db_names["date"],table_names["table6"],len(list))
+                        break         
+            
     def exit_user(self):
             
         id=input('Enter product_id:')
-
-        sql_connect=sqlite3.connect(db_names["user"])
-        cursor_sql=sql_connect.cursor()
-       
-        cursor_sql.execute("SELECT * FROM employers ")
-        table=cursor_sql.fetchall() 
-        
-        for item in table:
-                product_id=int(id)
-                if item[1]==product_id:
+    
 
                     
-                    sql_connect=sqlite3.connect(db_names["date"])
-                    cursor_sql=sql_connect.cursor()
-                    cursor_sql.execute("SELECT * FROM user_date ")
-                    items=cursor_sql.fetchall()
+        sql_connect=sqlite3.connect(db_names["date"])
+        cursor_sql=sql_connect.cursor()
+        cursor_sql.execute("SELECT * FROM user_date ")
+        items=cursor_sql.fetchall()
 
-                    for row in items:
-                        if  date == row[0] and item[1]==product_id:
-                            data = (time,product_id)
-                            sql_update_query =(f"""UPDATE user_date SET Exit_time =''
-                              WHERE user_id ={product_id} """)
-                            cursor_sql.execute(sql_update_query, data)
-                            break
+        for row in items:
+                
+            date == row[0] and row[1]==id
+            t=timer
+            update(t,id,db_names["date"],table_names["table6"])
+                    
+
+
+def update(list,id,db,table):
+
+
+    sql_connect=sqlite3.connect(db)
+    cursor_sql=sql_connect.cursor()
+   
+    cursor_sql.execute(f"UPDATE {table} SET Exit_time =(?) WHERE user_id ={id} ",(list,))
+    cursor_sql.execute(f"SELECT rowid, * FROM {table}")
+    sql_connect.commit()
+    sql_connect.close()
+
+
                         
-# li=[(1,),(2,),(3,),(4,)]                            
-# functions.delete_items("23456",db_names["date"],table_names["table6"])                        
+# li=[(2,),(3,),(4,)]                            
+# functions.delete_items(li,db_names["date"],table_names["table5"])                        
 
-#  Update TABLE_NAME set name = '',id = cast(NULLIF(id,'') as NVARCHAR)
+
 
                                  
 
 product=Date()
-product.exit_user()
-
+product.exit_product()
 
 
 
